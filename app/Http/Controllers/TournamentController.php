74d4137f44086fe21088156048f938ecde39216e
Hawk Dykes <hawkwdykes@gmail.com>
2023-05-31 20:47:17 -0500

    handle tournament building exceptions

--- app/Http/Controllers/TournamentController.php
+++ app/Http/Controllers/TournamentController.php
@@ -6,46 +6,33 @@ use Exception;
 use App\Models\User;
 use App\TournamentBuilder;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
 
 class TournamentController extends Controller
 {
     public function __invoke()
     {
-        $tournament = $this->buildTournament();
-        // dd($tournament);
-        return view('home', ['teams' => $tournament->getTeams()]);
+        $error = null;
+        $teams = new Collection();
 
+        try {
+            $tournament = $this->buildTournament();
+            $teams = $tournament->getTeams();
+        } catch (Exception $e) {
+            $errorMsg = $e->getMessage();
+            Log::info("Exception: $errorMsg");
+            $error = $errorMsg;
+        }
 
-        // try {
-        //     $tournament = $this->buildTournament();
-        // } catch (Exception $e) {
-            
-        // }
-
+        return view('home', ['error' => $error, 'teams' => $teams]);
     }
 
     private function buildTournament()
     {
-        // $players = User::OfPlayers()->get();
-        $players = $this->getPlayers(120);
+        $players = User::OfPlayers()->get();
         $tournamentBuilder = new TournamentBuilder($players);
         $tournamentBuilder->build();
 
         return $tournamentBuilder->getTournament();
-    }
-
-    private function getPlayers($num): Collection
-    {
-        $players = User::OfPlayers()->take($num)->get();
-
-        if ($num > 85) {
-            for ($i=0; $i < $num - 85; $i++) {
-                $base = $players[0];
-                $base->id = 91 + $i;
-                $players->push($base);
-            }
-        }
-
-        return $players;
     }
 }
